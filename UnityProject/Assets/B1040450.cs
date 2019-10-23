61db65d73d75ec47d2df0da17e81e05aac5edd00
hank634 <55218499+hank634@users.noreply.github.com>
2019-10-23 23:09:36 +0800

    5

---
+++ UnityProject/Assets/B1040450.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B1040450 : MonoBehaviour
+{
+    Animator m_Animator;
+
+    [Header("跑步開關")] bool run;//設定一個變數為run
+
+    void Start()
+    {
+       
+        m_Animator = gameObject.GetComponent<Animator>();
+       
+        run = false;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+            run = true;
+
+        else run = false;
+
+        if (run == false)
+            m_Animator.SetBool("跑步開關", false);
+
+        if (run == true)
+            m_Animator.SetBool("跑步開關", true);
+    }
+}
---
+++ UnityProject/Assets/B10404502.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B10404502 : MonoBehaviour
+{
+    Animator m_Animator;
+
+   
+
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.E))
+        {
+           m_Animator.SetTrigger("吃飯觸發器");
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+           
+            m_Animator.ResetTrigger("吃飯觸發器");
+
+        }
+
+    }
+}
